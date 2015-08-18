db7e73e77db6502f8955f4a08976110baad4b361
NilFurquim <nilfurquimds@gmail.com>
2015-08-18 09:43:31 -0300

    Multi-line string, condition

--- pyexamples/condition.py
+++ pyexamples/condition.py
@@ -1,2 +1,15 @@
 
 print('Hello World')
+
+sauce ='tomato'
+
+order_description = """
+Blah blah blah
+"""
+
+if sauce.startswith('toma'):
+    print("It's probably tomato..")
+else:
+    print("Not tomato")
+
+print(order_description)
